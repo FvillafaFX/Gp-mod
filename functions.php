68479610c7e0913e1159683004a15f1ea15d7d9a
FvillafaFX <133894864+FvillafaFX@users.noreply.github.com>
2024-05-21 13:16:51 -0500

    Update functions.php
    Fix duplicarted style.css bug

--- functions.php
+++ functions.php
@@ -2,24 +2,17 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
+// Enqueue styles for the child theme
+if ( !function_exists( 'child_theme_enqueue_styles' ) ) :
+    function child_theme_enqueue_styles() {
+        // Enqueue parent theme styles
+        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
-            $uri = get_template_directory_uri() . '/rtl.css';
-        return $uri;
+        // Enqueue child theme styles
+       // wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
     }
 endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-         
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array(  ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 11 );
+add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
 
 // END ENQUEUE PARENT ACTION
 

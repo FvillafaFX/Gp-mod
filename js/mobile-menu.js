733746d82928667a0dd80953d031a9f375815014
FvillafaFX <133894864+FvillafaFX@users.noreply.github.com>
2024-11-26 18:03:11 -0500

    Update mobile-menu.js
    Fix "Responsive slide" issue

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,30 +1,47 @@
-(function($) {
+(function ($) {
     function createMobileSubmenuDetails() {
-        $('.menu-item-has-children').each(function() {
+        $('.menu-item-has-children').each(function () {
             var $menuItem = $(this);
             var $submenu = $menuItem.children('.sub-menu');
             var $link = $menuItem.children('a');
 
             // Check if we're on a mobile device based on screen width
             if (window.matchMedia('(max-width: 1024px)').matches) {
-                // Only apply the wrapping if it hasn't been applied yet
-                if (!$link.parent().is('details')) {
-                    // Wrap the link and submenu within 'details' and 'summary'
-                    $link.add($submenu).wrapAll('<details></details>');
-                    $link.wrap('<summary></summary>');
+                // Apply the mobile submenu structure only if it hasn't been applied yet
+                if (!$menuItem.find('details').length) {
+                    // Create 'details' and 'summary' elements
+                    var $details = $('<details></details>');
+                    var $summary = $('<summary></summary>');
+
+                    $summary.append($link); // Move the link inside 'summary'
+                    $details.append($summary).append($submenu); // Add 'summary' and submenu into 'details'
+                    $menuItem.prepend($details); // Insert the 'details' back into the menu item
                 }
             } else {
-                // Revert changes if the screen is resized back to desktop width
-                if ($link.parent().is('summary')) {
-                    var $details = $link.closest('details');
-                    $link.unwrap('summary'); // Remove 'summary' wrapper from 'a'
-                    $link.next('.sub-menu').andSelf().unwrap('details'); // Remove 'details' wrapper
+                // Revert changes if resizing to desktop width
+                var $details = $menuItem.children('details');
+                if ($details.length) {
+                    var $summary = $details.children('summary');
+                    var $originalLink = $summary.children('a');
+
+                    // Move link and submenu back to their original positions
+                    $menuItem.prepend($originalLink);
+                    $menuItem.append($submenu);
+
+                    // Remove 'details' element
+                    $details.remove();
                 }
             }
         });
     }
 
     // Trigger the submenu toggle function on document ready and on resize
-    $(document).ready(createMobileSubmenuDetails);
-    $(window).on('resize', createMobileSubmenuDetails);
+    $(document).ready(function () {
+        createMobileSubmenuDetails();
+
+        // Handle window resizing
+        $(window).on('resize', function () {
+            createMobileSubmenuDetails();
+        });
+    });
 })(jQuery);

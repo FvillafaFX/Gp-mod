caf62b789b298178338c91c29acfb3807a87b11c
Eva Villafañe <97064704+evavillafae@users.noreply.github.com>
2025-06-05 19:16:08 -0500

    add 404 tempalte

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<style>
+.error-404 {width:100%;height:50vh;align-items:center;padding-top: 52px;padding-bottom: 52px;}
+.error-404 .section-wrap{flex-direction:column;text-align:center}
+.error-404 .btn{margin-right:auto;margin-left:auto}
+</style>
+<section class="error-404 not-found section">
+    <div class="section-wrap">
+        <h1 class="h1-style maintitle">Oops... page not found.</h1>
+        <p class="h2-style">But don't worry, you can find plenty of other things on our homepage.</p>
+        <a href="/" class="btn btn-primary">Go back to home</a>
+    </div>
+</section>
+
+<?php get_footer(); ?>
